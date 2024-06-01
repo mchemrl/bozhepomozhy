326d449a5924d0a74859326593970530cbd79549
airrinaaa <i.rychok@ukma.edu.ua>
2024-06-01 19:21:24 +0300

    i like to move it move it

--- core/src/screens/BottomMenu.java
+++ core/src/screens/BottomMenu.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -29,21 +30,21 @@ public class BottomMenu {
         table.setFillParent(true);
         table.center().bottom();
 
-        createTextButton("Shop", skin, shopAction, true, new ClickListener() {
+        createTextButton("Shop", skin, shopAction, true, false, new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 shopAction.run();
                 ((Game) Gdx.app.getApplicationListener()).setScreen(new ShopScreen());
             }
         });
-        createTextButton("Levels", skin, levelsAction, true, new ClickListener() {
+        createTextButton("Levels", skin, levelsAction, true, true, new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 levelsAction.run();
                 ((Game) Gdx.app.getApplicationListener()).setScreen(new Levels());
             }
         });
-        createTextButton("Arcade", skin, arcadeAction, true, new ClickListener() {
+        createTextButton("Arcade", skin, arcadeAction, true, false, new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 arcadeAction.run();
@@ -56,23 +57,28 @@ public class BottomMenu {
         Tween.registerAccessor(Actor.class, new ActorAccessor());
     }
 
-    private void createTextButton(String text, Skin skin, Runnable action, boolean animate, ClickListener clickListener) {
+    private void createTextButton(String text, Skin skin, Runnable action, boolean animate, boolean levelChoosen, ClickListener clickListener) {
         TextButton button = new TextButton(text, skin);
         button.getLabel().setAlignment(Align.center);
 
         // Додайте відступ зверху для тексту кнопки
         button.getLabelCell().padBottom(20);
 
+        if (levelChoosen) {
+            // Рух тексту на кнопці, якщо вкладка вибрана
+            animateButtonText(button.getLabel(), 7);
+        }
+
         button.addListener(clickListener);
 
         table.add(button).expandX().fillX().height(80).pad(0);
     }
 
-    private void animateButton(Actor button) {
-        Tween.to(button, ActorAccessor.Y, 0.5f)
-                .target(button.getY() + 50)
-                .ease(TweenEquations.easeOutExpo)
-                .start(tweenManager);
+    private void animateButtonText(Actor actor, int distance) {
+        actor.addAction(Actions.forever(Actions.sequence(
+                Actions.moveBy(0, distance, 0.3f),
+                Actions.moveBy(0, -distance, 0.3f)
+        )));
     }
 
     public void update(float delta) {
